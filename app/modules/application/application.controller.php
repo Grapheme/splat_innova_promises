06efa9aff81cd20ab72b1da7abcc11700a7f7caf
sngrl <reserved@mail.ru>
2014-12-01 15:58:29 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -638,6 +638,7 @@ class ApplicationController extends BaseController {
         $curl = curl_init('https://api.vk.com/method/users.get?user_ids=' . @$auth['user_id'] . '&fields=sex,bdate,city,country,photo_200,domain&v=5.27');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $s = curl_exec($curl);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept-Language: ru-RU;q=0.5'));
         curl_close($curl);
         $user = json_decode($s, true);
         $user = $user['response'][0];
