836e82f4db1f6b63b0ab7e6e80e2373d346ccb48
sngrl <reserved@mail.ru>
2014-12-02 17:17:35 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -694,11 +694,11 @@ class ApplicationController extends BaseController {
 
                 if ($hide_private_promises) {
 
-                    $tbl_alias_only_for_me = $query->join_field('only_for_my', 'only_for_my', function ($join, $value) use ($user) {
+                    $tbl_alias_only_for_me = $query->join_field('only_for_me', 'only_for_me', function ($join, $value) use ($user) {
                         $join->where($value, '=', NULL);
                     });
 
-                    $query->where('only_for_my', NULL);
+                    $query->where($tbl_alias_only_for_me.'.value', NULL);
                 }
 
             });
