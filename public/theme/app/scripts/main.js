744bd07d2a7a1080561a71ad0567c3db10d47a84
KostyaD <thedamaxstudio@gmail.com>
2014-12-10 20:07:08 +0300

    index page

--- public/theme/app/scripts/main.js
+++ public/theme/app/scripts/main.js
@@ -5,7 +5,8 @@ SplatSite.index = function() {
 			fit: 'cover',
 			width: '100%',
 			height: '600px',
-			autoplay: 5000,
+			autoplay: 6000,
+			transitionduration: 1000,
 			transition: 'dissolve',
 			arrows: false,
 			nav: false
@@ -13,7 +14,30 @@ SplatSite.index = function() {
 		$('.js-main-fotorama').fotorama(fsets);
 	}
 
+	var promises_fotorama = function() {
+		var fsets = {
+			width: '100%',
+			height: '600px',
+			autoplay: 1000,
+			transitionduration: 1000,
+			transition: 'dissolve',
+			arrows: false,
+			nav: false
+		};
+		$('.js-promises-fotorama').fotorama(fsets);
+	}
+
+	var promises_masonry = function() {
+		var container = document.querySelector('.js-promises');
+		var msnry = new Masonry( container, {
+		  columnWidth: 1,
+		  itemSelector: 'li'
+		});
+	}
+
 	main_fotorama();
+	promises_fotorama();
+	promises_masonry();
 }
 
 $(function(){
