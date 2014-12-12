13552c479c05f31b90c7ea34ada18e6ce5e4f388
sngrl <reserved@mail.ru>
2014-12-12 16:50:22 +0300

    fix

--- app/views/templates/site.blade.php
+++ app/views/templates/site.blade.php
@@ -15,7 +15,7 @@
 	@include(Helper::layout('head'))
 	@yield('style')
     </head>
-<body class="index-page">
+<body class="index-page bordered">
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
