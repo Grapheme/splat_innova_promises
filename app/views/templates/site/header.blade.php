b589b7b96f72ae20849995b5cd7f7994e18e99b4
sngrl <reserved@mail.ru>
2014-12-12 16:30:50 +0300

    fix

--- app/views/templates/site/header.blade.php
+++ app/views/templates/site/header.blade.php
@@ -1,6 +1,15 @@
   <header class="main-header">
     <div class="wrapper">
-      <div class="user-links"><a href="#"><span data-box="auth" class="js-open-box">Войти</span></a><a href="#"><span data-box="auth" class="js-open-box">Зарегистрироваться</span></a></div>
+      <div class="user-links">
+      @if (!isset($user) || !is_object($user) || $user->id)
+        <a href="#"><span data-box="auth" class="js-open-box">Войти</span></a>
+        <a href="#"><span data-box="auth" class="js-open-box">Зарегистрироваться</span></a>
+      @else
+        <a href="{{ URL::route('app.mainpage') }}"><span data-box="auth_" class="js-open-box_">Мой Профиль</span></a>
+        <a href="{{ URL::route('app.mainpage') }}"><span data-box="auth_" class="js-open-box_">Мои Обещания</span></a>
+        <a href="#"><span data-box="auth_" class="js-open-box_ logout">Выйти</span></a>
+      @endif
+      </div>
       <ul class="soc-links">
         <li><a href="#" class="soc-icon"><i class="fi icon-fb"></i></a></li>
         <li><a href="#" class="soc-icon"><i class="fi icon-vk"></i></a></li>
