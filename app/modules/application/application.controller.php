48ae12f9cdb8ae70e356140cd46f302d8758452e
sngrl <reserved@mail.ru>
2014-12-13 13:05:15 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -1112,7 +1112,7 @@ class ApplicationController extends BaseController {
         }
 
         /**
-         * OK не отдают е-мейл
+         * OK теперь отдают е-мейл (нужно было запрашивать у Ozhiganov Valery <valery.ozhiganov@corp.mail.ru>)
          */
         $curl = curl_init('http://api.odnoklassniki.ru/fb.do?access_token=' . $auth['access_token'] . '&application_key=' . $AUTH['application_key'] . '&method=users.getCurrentUser&sig=' . md5('application_key=' . $AUTH['application_key'] . 'method=users.getCurrentUser' . md5($auth['access_token'] . $AUTH['client_secret'])));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
--- app/views/templates/site/send_invite.blade.php
+++ app/views/templates/site/send_invite.blade.php
@@ -7,6 +7,7 @@
 
 @section('content')
 
+    @if (0)
 
         <big>{{ $user_name }}</big>
 
@@ -18,14 +19,42 @@
             <button>Пригласить</button>
         </form>
 
-    <hr/>
+        <hr/>
 
-    DEBUG:
-    {{ Helper::d(@$_SESSION) }}
-    {{ Helper::d(@$_COOKIE) }}
-    {{ Helper::ta_(@$promises) }}
+        DEBUG:
+        {{ Helper::d(@$_SESSION) }}
+        {{ Helper::d(@$_COOKIE) }}
+        {{ Helper::ta_(@$promises) }}
 
-    <div id="fb-root"></div>
+    @endif
+
+      <div class="profile-page">
+        <div class="wrapper">
+          <div class="profile-card">
+            <div style="background-image: url(http://img0.liveinternet.ru/images/attach/c/6/102/827/102827412_1346919545_0107400x320.jpg);" class="profile-photo"></div>
+            <div class="profile-info profile-invite">
+              <div class="info-cont">
+                <div class="name"><span>{{ $user_name }}</span></div>
+                <div class="age">28 лет</div>
+                <div class="invite-info">
+                  <p>Ваш друг {{-- из Facebook --}} не дал еще ни одного обещания. </p>
+                  <p>Пригласите вашего друга и расскажите ему о том, почему так важно сдерживать данные обещания.</p>
+                  <div class="inv-form">
+                    <div class="inv-btn js-inv-btn-cont"><a href="#" class="us-btn js-inv-btn">Пригласить друга</a></div>
+                    <div style="display: none;" class="form js-inv-form">
+                      <form action="{{ URL::route('app.send_invite_message') }}" method="POST">
+                          <input name="email" placeholder="E-mail друга" class="us-input">
+                          <button class="us-btn">Пригласить</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
 @stop
 
