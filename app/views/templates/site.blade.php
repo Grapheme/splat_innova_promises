6d2fbcbdb63d07351bff7ab51ef56ef5a9587e67
sngrl <reserved@mail.ru>
2014-12-15 14:06:17 +0300

    fix

--- app/views/templates/site.blade.php
+++ app/views/templates/site.blade.php
@@ -13,16 +13,27 @@
 <!DOCTYPE html>
     <head>
 	@include(Helper::layout('head'))
-	@yield('style')
+
+    @section('style')
+    @show
+
     </head>
 <body class="index-page">
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     @include(Helper::layout('header'))
-    @yield('content', @$content)
+
+    @section('content')
+        {{ @$content }}
+    @show
+
     @include(Helper::layout('footer'))
     @include(Helper::layout('scripts'))
-    @yield('overlays')
-    @yield('scripts')
+
+    @section('overlays')
+    @show
+
+    @section('scripts')
+    @show
 </body>
