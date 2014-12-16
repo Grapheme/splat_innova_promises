f357b638af52eec828a430dd1d518f6b384ade79
KostyaD <thedamaxstudio@gmail.com>
2014-12-16 13:40:53 +0300

    fix

---
+++ app/views/templates/site/restore_password_success.blade.php
@@ -0,0 +1,26 @@
+@extends(Helper::layout())
+
+
+@section('style')
+@stop
+
+
+@section('content')
+
+	<div class="wrapper">
+	  <div class="normal-wrapper">
+	    <div class="normal-title">Восстановление пароля</div>
+	    <div class="normal-text">
+	      <p>Новый пароль успешно установлен. Мы отправили его вам на электронную почту.</p>
+	    </div>
+	  </div>
+	</div>
+
+    
+
+@stop
+
+
+@section('scripts')
+
+@stop
