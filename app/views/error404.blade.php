7986557f98ae80c3702e98ed310a6f98e01b09ac
sngrl <reserved@mail.ru>
2014-12-16 19:43:10 +0300

    fix

--- app/views/error404.blade.php
+++ app/views/error404.blade.php
@@ -7,7 +7,15 @@
 
 @section('content')
 
-404
+    <div class="error404">
+        <div class="error404-cont">
+            <div class="icon">
+                <div class="fi icon-sad"></div>
+            </div>
+            <div class="title">404–ошибка</div>
+            <div class="text">Страница не найдена.<br>Адрес набран неверно, или страница была удалена.<br>Перейти <a href="{{ URL::route('app.mainpage') }}">на главную</a>.</div>
+        </div>
+    </div>
 
 @stop
 
