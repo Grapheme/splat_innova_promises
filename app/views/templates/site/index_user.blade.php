7e721b2a590a13b1e68e2259f46672873a36e73b
sngrl <reserved@mail.ru>
2014-12-16 14:48:07 +0300

    fix

--- app/views/templates/site/index_user.blade.php
+++ app/views/templates/site/index_user.blade.php
@@ -19,6 +19,49 @@
 @stop
 
 
+@section('promise')
+    <li class="promise-item type-{{ $promise->style_id }}" data-finish="{{ $promise->time_limit }}">
+        <a href="{{ URL::route('app.promise', $promise->id) }}" class="promise-content">
+            <div class="title">
+                {{ $promise->name }}
+            </div>
+            <div class="bottom-block">
+                <div class="top-floor">
+                    @if ($promise->only_for_me)
+                        <div class="eye eye-cross" data-tooltip="Обещание защищено настройками приватности и видимо только вам."></div>
+                    @else
+                        <div class="eye"></div>
+                        @endif
+                                <!-- <div class="eye{{ ( $promise->only_for_me ? ' eye-cross' : '') }}"></div> -->
+                </div>
+                <div class="bottom-floor">
+                    <!--
+                    <div class="views">15</div>
+                    -->
+                    <div class="comments" data-tooltip="{{ (int)$promise->comments_count }} человека оставили свои комментарии">{{ (int)$promise->comments_count }}</div>
+
+                    <?
+                    $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
+                    ?>
+                    @if ($failed)
+                        <div class="unsmile" data-tooltip="Обещание не было выполнено.">
+                            <!-- <i class="fi icon-unsmile"></i> -->
+                        </div>
+                    @elseif ($promise->finished_at)
+                        <div class="smile" data-tooltip="Обещание выполнено.">
+                            <!-- <i class="fi icon-smile"></i> -->
+                        </div>
+                    @else
+                        <!-- <div class="time">02:01:23</div> -->
+                    @endif
+
+                </div>
+            </div>
+        </a>
+    </li>
+@stop
+
+
 @section('content')
 
     @if (0)
@@ -155,45 +198,7 @@
                     $promise->style_id = $styles[array_rand($styles)];
                 }
             ?>
-              <li class="promise-item type-{{ $promise->style_id }}" data-finish="{{ $promise->time_limit }}">
-                <a href="{{ URL::route('app.promise', $promise->id) }}" class="promise-content">
-                  <div class="title">
-                    {{ $promise->name }}
-                  </div>
-                  <div class="bottom-block">
-                    <div class="top-floor">
-                      @if ($promise->only_for_me)
-                        <div class="eye eye-cross" data-tooltip="Обещание защищено настройками приватности и видимо только вам."></div>
-                      @else
-                        <div class="eye"></div>
-                      @endif
-                            <!-- <div class="eye{{ ( $promise->only_for_me ? ' eye-cross' : '') }}"></div> -->
-                    </div>
-                    <div class="bottom-floor">
-                      <!--
-                      <div class="views">15</div>
-                      -->
-                      <div class="comments" data-tooltip="{{ (int)$promise->comments_count }} человека оставили свои комментарии">{{ (int)$promise->comments_count }}</div>
-
-                        <?
-                        $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
-                        ?>
-                        @if ($failed)
-                            <div class="unsmile" data-tooltip="Обещание не было выполнено.">
-                                <!-- <i class="fi icon-unsmile"></i> -->
-                            </div>
-                        @elseif ($promise->finished_at)
-                            <div class="smile" data-tooltip="Обещание выполнено.">
-                                <!-- <i class="fi icon-smile"></i> -->
-                            </div>
-                        @else
-                            <!-- <div class="time">02:01:23</div> -->
-                        @endif
-
-                    </div>
-                  </div>
-                </a>
-              </li>
+                @yield('promise')
 
                 @if ($p == 2)
                     @yield('koe-chto')
