8efaee47af5bd3fb36ae6e21e0f4b047595f2d9b
KostyaD <thedamaxstudio@gmail.com>
2014-12-16 12:06:07 +0300

    restore password

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -51,21 +51,9 @@
                 <input type="password" name="pass" placeholder="Пароль">
               </div>
               <div class="btns">
-                <button class="us-btn">Войти</button><a href="{{ URL::route('app.restore_password') }}" class="right-link js-form-pass">Забыли пароль?</a>
+                <button class="us-btn">Войти</button><a href="{{ URL::route('app.restore_password') }}" class="right-link js-change-box" data-box="restore">Забыли пароль?</a><a href="#" class="right-link js-change-box" data-box="reg">Регистрация</a>
               </div>
             </form>
-
-          </div>
-          <div data-type="pass" style="display: none;" class="form-inputs js-pop-form">
-            <form>
-              <div class="input-cont">
-                <input placeholder="Эл. почта">
-              </div>
-              <div class="btns">
-                <button class="us-btn">Восстановить</button>
-              </div>
-            </form>
-          </div>
         </div>
       </div>
       <div class="wrapper">
@@ -75,6 +63,21 @@
           <li class="about-sav"><span class="icon"></span><span class="text">Вы сможете сохранить ваше обещание, а мы будем напоминать вам о нем.</span></li>
           <li class="about-new"><span class="icon"></span><span class="text">В новогоднюю ночь сбываются чудеса, а люди во всем мире дают друг другу обещания, которые порой так трудно исполнить.</span></li>
         </ul>
+      </div>
+    </div>
+    <div data-box="restore" class="popup auth-popup js-pop-up"><a href="#" class="popup-close js-pop-close"><span></span></a>
+      <div class="restore-form">
+        <div class="form-inputs">
+          <form>
+            <div class="input-title">Восстановление пароля</div>
+            <div class="input-cont">
+              <input placeholder="Укажите е-мейл" class="us-input">
+            </div>
+            <div class="btns">
+              <button class="us-btn">Восстановить</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
     <div data-box="reg" class="popup auth-popup js-pop-up"><a href="#" class="popup-close js-pop-close"><span></span></a>
