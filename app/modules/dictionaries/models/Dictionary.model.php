f8533e0b419091b4319a6676d486a2938e40594c
sngrl <reserved@mail.ru>
2014-12-17 12:46:23 +0300

    fix

--- app/modules/dictionaries/models/Dictionary.model.php
+++ app/modules/dictionaries/models/Dictionary.model.php
@@ -169,7 +169,7 @@ class Dictionary extends BaseModel {
                 $list = array();
                 if (isset($col->$listed_key) && count($col->$listed_key))
 
-                    Helper::ta($col->$listed_key);
+                    Helper::d($col->$listed_key);
                     foreach ($col->$listed_key as $e => $el) {
                         Helper::ta($e);
                         Helper::ta($el);
