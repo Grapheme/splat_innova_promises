8634e3abf4334bed9e73ac77bf35b9ceb11046c0
sngrl <reserved@mail.ru>
2014-12-17 11:38:15 +0300

    fix

--- app/views/templates/site/promise_inc.blade.php
+++ app/views/templates/site/promise_inc.blade.php
@@ -16,7 +16,8 @@
                 <!--
                 <div class="views">15</div>
                 -->
-                <div class="comments" data-tooltip="{{ (int)$promise->comments_count }} человека оставили свои комментарии">{{ (int)$promise->comments_count }}</div>
+
+                <div class="comments" data-tooltip="{{ trans_choice(':count комментарий|:count комментария|:count комментариев', (int)$promise->comments_count, array(), 'ru') }} к этому обещанию.">{{ (int)$promise->comments_count }}</div>
 
                 <?
                 $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
