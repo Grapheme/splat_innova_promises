381b0049534fe2e4aa2cdc4f8e9c9f974d5883b5
KostyaD <thedamaxstudio@gmail.com>
2014-12-17 11:36:30 +0300

    get back

--- app/views/templates/site/promise_inc.blade.php
+++ app/views/templates/site/promise_inc.blade.php
@@ -1,27 +1,3 @@
-<?php
-
-    function getNumEnding($number, $endingArray)
-    {
-        $number = $number % 100;
-        if ($number>=11 && $number<=19) {
-            $ending=$endingArray[2];
-        }
-        else {
-            $i = $number % 10;
-            switch ($i)
-            {
-                case (1): $ending = $endingArray[0]; break;
-                case (2):
-                case (3):
-                case (4): $ending = $endingArray[1]; break;
-                default: $ending=$endingArray[2];
-            }
-        }
-        return $ending;
-    }
-
-?>
-
 <li class="promise-item type-{{ $promise->style_id }}" @if(@promise_type == 'active') data-finish="{{ $promise->time_limit }}" @endif>
     <a href="{{ URL::route('app.promise', $promise->id) }}" class="promise-content">
         <div class="title">
@@ -40,7 +16,7 @@
                 <!--
                 <div class="views">15</div>
                 -->
-                <div class="comments" data-tooltip="{{ (int)$promise->comments_count }} {{getNumEnding((int)$promise->comments_count, array('комментарий, комментария, комментариев'))}} к этой записи">{{ (int)$promise->comments_count }}</div>
+                <div class="comments" data-tooltip="{{ (int)$promise->comments_count }} человека оставили свои комментарии">{{ (int)$promise->comments_count }}</div>
 
                 <?
                 $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
