a64cd84746c3444d01a7abc3f630b7c3702560d0
sngrl <reserved@mail.ru>
2014-12-17 13:20:49 +0300

    fix

--- app/views/templates/site/promise.blade.php
+++ app/views/templates/site/promise.blade.php
@@ -160,9 +160,11 @@
                         <div class="text">
                             {{ $comment->comment_text }}
                         </div>
+                        @if ($comment->user_id == $auth_user->id || $promise->user_id == $auth_user->id)
                         <div class="text">
                             <a href="?do=delete_comment&id={{ $comment->id }}">удалить</a>
                         </div>
+                        @endif
                     </div>
                 </li>
                 @endforeach
