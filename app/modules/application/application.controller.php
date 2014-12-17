eb67300467929f17a8e0d48b10487efc04b96cc3
sngrl <reserved@mail.ru>
2014-12-17 12:13:09 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -258,7 +258,7 @@ class ApplicationController extends BaseController {
 
                         $existing_friends_temp->load('dicval.fields');
                         #Helper::tad($existing_friends_temp);
-                        $existing_friends_temp = DicVal::extracts($existing_friends_temp);
+                        $existing_friends_temp = DicVal::extracts($existing_friends_temp->dicval);
 
                         $existing_friends_list = Dic::makeLists($existing_friends_temp, null, 'dicval_id', 'value');
                         $existing_friends_avatars = Dic::makeLists($existing_friends_temp, null, 'dicval_id', 'avatar');
