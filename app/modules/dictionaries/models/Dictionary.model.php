f641352f81ba16494d77689d9bba8182c54b38d0
sngrl <reserved@mail.ru>
2014-12-17 12:59:27 +0300

    fix

--- app/modules/dictionaries/models/Dictionary.model.php
+++ app/modules/dictionaries/models/Dictionary.model.php
@@ -188,9 +188,9 @@ class Dictionary extends BaseModel {
                         #Helper::d($col->$listed_key);
                         #$col->$listed_key->attributes[$key]
                         if ($key != '') {
-                            $lists[$col->$listed_key->attributes[$value]] = $col->$listed_key->attributes[$key];
+                            $lists[$col->$listed_key->attributes[$key]] = $col->$listed_key->attributes[$value];
                         } else {
-                            $lists[] = $col->$listed_key->attributes[$key];
+                            $lists[] = $col->$listed_key->attributes[$value];
                         }
                     }
                     #Helper::dd($list);
