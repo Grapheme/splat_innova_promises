c2bff8dd57eac33d3679d83306e4fb108d248e53
sngrl <reserved@mail.ru>
2014-12-18 17:58:30 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -2146,6 +2146,7 @@ class ApplicationController extends BaseController {
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_POST, 1);
         @curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+        curl_setopt($curl, CURLOPT_SAFE_UPLOAD, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $s = curl_exec($curl);
         curl_close($curl);
