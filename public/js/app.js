457ad3591e68d03d7d43c4d2eb579f2a96db3a2c
sngrl <reserved@mail.ru>
2014-12-18 17:12:38 +0300

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -625,8 +625,8 @@ $("#promise-form").validate({
                 $.ajax({
                     url: r.response.upload_url,
                     type: 'POST',
-                    dataType: 'json',
-                    //dataType: 'jsonp',
+                    //dataType: 'json',
+                    dataType: 'jsonp',
                     //jsonp: 'callback',
                     data: { photo: 'http://mypromises.ru/promise_card.jpg' }
                 })
