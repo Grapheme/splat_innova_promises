a45bb3a1dc521b9e25824e48cc19f938d07ac22e
sngrl <reserved@mail.ru>
2014-12-18 18:36:47 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -1098,8 +1098,10 @@ class ApplicationController extends BaseController {
 
 
             if (
-                (isset($_COOKIE['user_token']) && $_COOKIE['user_token'] != 'logout') || isset($_SESSION['user_token'])
+                (!isset($_COOKIE['user_token']) || $_COOKIE['user_token'] != 'logout') && !isset($_SESSION['user_token'])
             ) {
+                ##
+            } else {
 
                 if (Input::get('debug') == 1 || 1) {
                     Helper::d('$_COOKIE');
