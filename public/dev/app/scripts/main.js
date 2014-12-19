600b60ed09a2e2592f41a976d6c9263c47d07187
KostyaD <thedamaxstudio@gmail.com>
2014-12-19 13:25:50 +0300

    test

--- public/dev/app/scripts/main.js
+++ public/dev/app/scripts/main.js
@@ -286,6 +286,7 @@ SplatSite.CountDown = function(elem) {
 			date_str = date_str.replace('-', '/');
 		}
 		$(this).countdown(date_str, function(event){
+			console.log(event);
 			var days_name = declOfNum(parseInt(event.strftime('%-D')), ['день', 'дня', 'дней']);
 			$(this).html(event.strftime('<span class="time-day"><span>%-D</span> ' + days_name + '</span><span class="time-time">%H:%M:%S</span>'));
 		});
