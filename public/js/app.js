ba91728aa86d2626775a052d04a06b2ddce0cb4f
sngrl <reserved@mail.ru>
2015-01-23 13:32:12 +0300

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -665,7 +665,7 @@ $("#promise-form").validate({
         //console.log(form);
         var current_form = form;
         $(current_form).find('.btn-cont .us-btn').attr('disabled', 'disabled');
-        return false;
+        //return false;
 
         var only_for_me = $('input[name=only_for_me]').prop("checked");
 
