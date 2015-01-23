31c276a01b2c248a059df45dfb6106bdb606573a
KostyaD <thedamaxstudio@gmail.com>
2015-01-23 12:48:27 +0300

    fixes

--- public/js/app.js
+++ public/js/app.js
@@ -663,8 +663,8 @@ $("#promise-form").validate({
     submitHandler: function(form) {
 
         //console.log(form);
-
         var current_form = form;
+        current_form.find('.btn-cont .us-btn').attr('disabled', 'disabled');
 
         var only_for_me = $('input[name=only_for_me]').prop("checked");
 
