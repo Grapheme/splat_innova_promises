5aed72b7e3885d01b5a928f341bedc726c0a4edc
sngrl <reserved@mail.ru>
2015-03-10 19:03:44 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -147,8 +147,8 @@ class ApplicationController extends BaseController {
              * Берем все эти обещания
              */
             $mainpage_promises = Dic::valuesBySlug('promises', function($query) use ($ids) {
-                $query->whereIn('id', $ids);
-                #$query->order_by_field('promise_of_the_week', 'DESC');
+                $query->whereIn('dictionary_values.id', $ids);
+                $query->order_by_field('promise_of_the_week', 'DESC');
             });
             $mainpage_promises = DicVal::extracts($mainpage_promises, null, 1, 1);
 
