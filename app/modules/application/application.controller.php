7a4a00b152092837a3380ae70a4896d9bc0f668e
sngrl <reserved@mail.ru>
2015-03-10 19:06:43 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -149,9 +149,9 @@ class ApplicationController extends BaseController {
             $mainpage_promises = Dic::valuesBySlug('promises', function($query) use ($ids) {
                 #$query->whereIn(DB::raw('`dictionary_values`.`id`'), $ids);
                 $query->whereIn(DB::raw('`dictionary_values`.`id`'), $ids);
-                $query->order_by_field('promise_of_the_week', 'DESC');
+                #$query->order_by_field('promise_of_the_week', 'DESC');
             });
-            Helper::smartQueries(1); die;
+            #Helper::smartQueries(1); die;
             $mainpage_promises = DicVal::extracts($mainpage_promises, null, 1, 1);
 
             if (count($mainpage_promises)) {
