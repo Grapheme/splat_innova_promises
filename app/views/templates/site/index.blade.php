53598e23569e9dcda37963f375d950487740e646
KostyaD <thedamaxstudio@gmail.com>
2015-07-21 13:35:27 +0300

    fixes

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -179,7 +179,7 @@
                                           <div class="info-cont">
                                               <a class="comments-amount">{{ trans_choice(':count комментарий|:count комментария|:count комментариев', $promise_comments_count, array(), 'ru') }}</a>
                                               <div class="pr-title js-promise-text">{{ mb_strtoupper($promise->name) }}</div>
-                                              <div class="user-info"><a href="#" style="background-image: url({{ $puser->avatar }})" class="user-photo"></a>
+                                              <div class="user-info"><a style="background-image: url({{ $puser->avatar }})" class="user-photo"></a>
                                                   <div class="user-text">
                                                       <div class="name">{{ $puser->name }}</div>
                                                       <div class="city">{{ $puser->city }}</div>
@@ -230,7 +230,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">ВЫЙТИ ЗАМУЖ В ЭТОМ ГОДУ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/b0c82b4fbbcc66aef2b56083809596a2.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/b0c82b4fbbcc66aef2b56083809596a2.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Кристина</div>
                           <div class="city">Киев</div>
@@ -254,7 +254,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">БЫТЬ ЛУЧШИМ ОТЦОМ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/e9409610fb556cf414e6dc6997bd2cea.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/e9409610fb556cf414e6dc6997bd2cea.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Артем</div>
                           <div class="city">Москва</div>
@@ -278,7 +278,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">НАУЧИТЬСЯ ТАНЦЕВАТЬ САЛЬСУ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/d98cd811d0ff77f1349dc4e76168689a.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/d98cd811d0ff77f1349dc4e76168689a.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Ксения</div>
                           <div class="city">Питер</div>
@@ -302,7 +302,7 @@
                   <div class="promise-cont type-green">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">НЕ РАССТРАИВАТЬСЯ ПО ПУСТЯКАМ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://cs624627.vk.me/v624627062/225/lKcpY5Jh2jY.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://cs624627.vk.me/v624627062/225/lKcpY5Jh2jY.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Юлия Гафарова</div>
                           <div class="city">Уфа</div>
@@ -326,7 +326,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">ЗАНЯТЬ ПРИЗОВОЕ МЕСТО НА ВОКАЛЬНОМ КОНКУРСЕ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/64b4ea9f026a25675e7f17b9df9a8f23.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/64b4ea9f026a25675e7f17b9df9a8f23.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Анна</div>
                           <div class="city">Владимир</div>
@@ -350,7 +350,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">БЫТЬ СЧАСТЛИВОЙ КАЖДУЮ МИНУТУ 2015 ГОДА</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://cs607919.vk.me/v607919721/61cb/ZuNjvJqdZ14.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://cs607919.vk.me/v607919721/61cb/ZuNjvJqdZ14.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Виктория Михайлова</div>
                           <div class="city">Ростов-на-Дону</div>
@@ -374,7 +374,7 @@
                   <div class="promise-cont type-green">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">СТАТЬ ХОРОШИМ ВРАЧОМ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://cs621226.vk.me/v621226487/8722/n4fAxxaN1Ig.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://cs621226.vk.me/v621226487/8722/n4fAxxaN1Ig.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Никита Артемов</div>
                           <div class="city">Заринск</div>
@@ -398,7 +398,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">ВЛЮБИТЬСЯ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/3356e062b0b4be1b8ef9604ab12ea7b2.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/3356e062b0b4be1b8ef9604ab12ea7b2.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Карина</div>
                           <div class="city">Сальск</div>
@@ -422,7 +422,7 @@
                   <div class="promise-cont type-green">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">НАУЧИТЬСЯ ИГРАТЬ НА ГИТАРЕ "SMOKE ON THE WATER"</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://cs411121.vk.me/v411121773/9d1a/SIxpfGvfpT0.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://cs411121.vk.me/v411121773/9d1a/SIxpfGvfpT0.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Павел Ханов</div>
                           <div class="city">Екатеринбург</div>
@@ -446,7 +446,7 @@
                   <div class="promise-cont type-aqua">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">ПРИХОДИТЬ НА РАБОТУ В ХОРОШЕМ НАСТРОЕНИИ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/bf0107c3df32ad80e336171fe425f5dd.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/bf0107c3df32ad80e336171fe425f5dd.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Владимир Манычев</div>
                           <div class="city">Москва</div>
@@ -470,7 +470,7 @@
                   <div class="promise-cont type-blue">
                     <div class="info-cont"><a class="comments-amount">0 комментариев</a>
                       <div class="pr-title js-promise-text">УСТРОИТЬ ОГРОМНЫЙ ПРАЗДНИК НА ДЕНЬ РОЖДЕНИЯ ДОЧКИ</div>
-                      <div class="user-info"><a href="#" style="background-image: url(http://mypromises.ru/uploads/avatar/49cf97b4e4171eaead6babef89003fbc.jpg)" class="user-photo"></a>
+                      <div class="user-info"><a style="background-image: url(http://mypromises.ru/uploads/avatar/49cf97b4e4171eaead6babef89003fbc.jpg)" class="user-photo"></a>
                         <div class="user-text">
                           <div class="name">Юлианна Никулина</div>
                           <div class="city">Москва</div>
