e10cfe4d02b36938e5f93d47cae863f4200c403a
sngrl <reserved@mail.ru>
2015-08-25 18:36:48 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -850,7 +850,7 @@ class ApplicationController extends BaseController {
         $result = $this->genPromiseImage($promise->id, 'Я обещаю ' . trim($promise_text), $style_id, $this->user->avatar);
         $img_path = $result['path'];
 
-        /
+        /**
          * Отсылаем уведомление на почту
          */
         if (@$this->user->notifications['promise_status'] && filter_var($this->user->email, FILTER_VALIDATE_EMAIL)) {
