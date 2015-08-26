85f9a5a72f8272ac7f1cc6a0600b821f2c02bb98
sngrl <reserved@mail.ru>
2015-08-26 17:09:39 +0300

    fix

--- app/config/dic/achievements.php
+++ app/config/dic/achievements.php
@@ -9,6 +9,15 @@ return array(
                 'title' => 'Текст ачивки',
                 'type' => 'textarea',
             ),
+            'image' => array(
+                'title' => 'Иконка',
+                'type' => 'image',
+                'params' => array(
+                    'maxFilesize' => 1, // MB
+                    #'acceptedFiles' => 'image/*',
+                    #'maxFiles' => 2,
+                ),
+            ),
             'count' => array(
                 'title' => 'Количество обещаний',
                 'type' => 'text',
