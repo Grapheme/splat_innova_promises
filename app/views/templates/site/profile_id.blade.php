9ae4e925116d328c9a25e552487eec258b391d00
sngrl <reserved@mail.ru>
2015-08-28 13:00:31 +0300

    fix

--- app/views/templates/site/profile_id.blade.php
+++ app/views/templates/site/profile_id.blade.php
@@ -4,16 +4,14 @@ $ach_list = [];
 if (isset($achievements) && count($achievements)) {
     foreach ($achievements as $achievement) {
         $ach_list[] = [
-            'name' => $achievement->name,
-            'desc' => $achievement->desc,
-            'icon' => isset($achievement->image) && is_object($achievement->image) ? $achievement->image->full() : '',
+                'name' => $achievement->name, 'desc' => $achievement->desc, 'icon' => isset($achievement->image) && is_object($achievement->image) ? $achievement->image->full() : '',
         ];
     }
 }
 ?>
 {{--<!-- {{ Helper::ta(Config::get('site.achievements')) }} -->--}}
 {{--<!-- {{ Helper::ta($achievements) }} -->--}}
-<!--{{  var_dump($subscribed) }}-->
+        <!--{{  var_dump($subscribed) }}-->
 
 @section('style')
 @stop
@@ -25,7 +23,10 @@ if (isset($achievements) && count($achievements)) {
             <div class="logo"></div>
             <div class="text">
                 <p>КАЖДЫЙ РАЗ, ВЫПОЛНЯЯ ОБЕЩАНИЯ,<br>ВЫ СТАНОВИТЕСЬ СИЛЬНЕЕ.</p>
-                <p>МЫ ТОЖЕ ХОТИМ <a href="#" class="js-open-box" data-box="promo" onclick="ga('send', 'event', 'brand', 'friend');">ДАТЬ ВАМ ОБЕЩАНИЕ</a></p>
+
+                <p>МЫ ТОЖЕ ХОТИМ
+                    <a href="#" class="js-open-box" data-box="promo" onclick="ga('send', 'event', 'brand', 'friend');">ДАТЬ ВАМ ОБЕЩАНИЕ</a>
+                </p>
             </div>
         </div>
     </li>
@@ -36,11 +37,13 @@ if (isset($achievements) && count($achievements)) {
 
     @if (0)
 
-        <big><a href="{{ $user->identity }}" target="_blank">{{ $user->name }}</a></big>
+        <big>
+            <a href="{{ $user->identity }}" target="_blank">{{ $user->name }}</a>
+        </big>
 
-        <br/>
+        <br />
 
-        Обещаний: {{ @count($promises) }}<br/>
+        Обещаний: {{ @count($promises) }}<br />
 
         @if (isset($promises) && is_object($promises) && count($promises))
 
@@ -49,19 +52,21 @@ if (isset($achievements) && count($achievements)) {
             {{ Helper::ta($promises) }}
 
             <ul>
-            @foreach ($promises as $promise)
-                {{ Helper::ta_($promise) }}
-                <?
-                if ($promise->only_for_me)
-                    continue;
-                ?>
-                <li><a href="{{ URL::route('app.promise', $promise->id) }}">{{ $promise->name }}</a></li>
-            @endforeach
+                @foreach ($promises as $promise)
+                    {{ Helper::ta_($promise) }}
+                    <?
+                    if ($promise->only_for_me)
+                        continue;
+                    ?>
+                    <li>
+                        <a href="{{ URL::route('app.promise', $promise->id) }}">{{ $promise->name }}</a>
+                    </li>
+                @endforeach
             </ul>
         @else
 
             <div class="wrapper">
-              <div class="us-text">Этот пользователь уже зарегистрирован на сайте, но пока не давал обещаний.</div>
+                <div class="us-text">Этот пользователь уже зарегистрирован на сайте, но пока не давал обещаний.</div>
             </div>
 
         @endif
@@ -75,32 +80,35 @@ if (isset($achievements) && count($achievements)) {
         $default_avatar = '/theme/images/woman.png';
     ?>
 
-      <div class="profile-page">
+    <div class="profile-page">
         <div class="wrapper">
-          <div class="profile-card">
-            <div style="background-image: url({{ $user->avatar ?: $default_avatar }});" class="profile-photo"></div>
-            <div class="profile-info">
-              <div class="info-cont">
-                <div class="name"><span>{{ $user->name }}</span></div>
-                @if ($user->years_old && 0)
-                  <div class="age">
-                    {{ trans_choice(':count год|:count года|:count лет', $user->years_old, array(), 'ru') }}
-                  </div>
+            <div class="profile-card">
+                <div style="background-image: url({{ $user->avatar ?: $default_avatar }});" class="profile-photo"></div>
+                <div class="profile-info">
+                    <div class="info-cont">
+                        <div class="name"><span>{{ $user->name }}</span></div>
+                        @if ($user->years_old && 0)
+                            <div class="age">
+                                {{ trans_choice(':count год|:count года|:count лет', $user->years_old, array(), 'ru') }}
+                            </div>
+                        @endif
+                        @if ($user->city)
+                            {{ $user->city }}
+                        @endif
+                        <div class="achives js-achives"></div>
+                    </div>
+                </div>
+                @if ($subscribed !== null)
+                    <div class="btn-cont">
+                        @if ($subscribed)
+                            <a href="#" class="us-btn btn-unfollow">Отписаться</a>
+                        @else
+                            <a href="#" class="us-btn">Подписаться</a>
+                        @endif
+                    </div>
                 @endif
-                @if ($user->city)
-                  {{ $user->city }}
-                @endif
-                <div class="achives js-achives"></div>
-              </div>
             </div>
-            {{--
-              <div class="btn-cont">
-                <a href="#" class="us-btn">Подписаться</a>
-                <a href="#" class="us-btn btn-unfollow">Отписаться</a>
-              </div>
-            --}}
-          </div>
-          <div class="promises-title us-title">Обещания</div>
+            <div class="promises-title us-title">Обещания</div>
         </div>
 
 
@@ -110,83 +118,89 @@ if (isset($achievements) && count($achievements)) {
             $p = 0;
             ?>
             <ul class="promises-list">
-            @foreach ($promises as $promise)
-            <?
-            ++$p;
-            if (!$promise->style_id) {
-                $styles = array('green', 'aqua', 'yellow', 'blue', 'pink');
-                $promise->style_id = $styles[array_rand($styles)];
-            }
-            ?>
-              <li class="promise-item type-{{ $promise->style_id }}" data-finish="{{ $promise->time_limit }}">
-                <ul class="soc-block">
-                  <li><a onclick="ga('send', 'event', 'like', 'facebook');" href="http://www.facebook.com/sharer.php?u={{ URL::route('app.promise', $promise->id) }}" class="soc-icon" target="_blank"><i class="fi icon-fb"></i></a></li>
-                  <li><a onclick="ga('send', 'event', 'like', 'vkontakte');" href="http://vk.com/share.php?url={{ URL::route('app.promise', $promise->id) }}&event=button_share" class="soc-icon" target="_blank"><i class="fi icon-vk"></i></a></li>
-                  <li><a onclick="ga('send', 'event', 'like', 'odnoklassniki');" href="http://www.odnoklassniki.ru/dk?st.cmd=addShare&st._surl={{ URL::route('app.promise', $promise->id) }}" class="soc-icon" target="_blank"><i class="fi icon-ok"></i></a></li>
-                </ul>
-                <a href="{{ URL::route('app.promise', $promise->id) }}" class="promise-content">
-                  <div class="title">
-                    {{ $promise->name }}
-                  </div>
-                  <div class="bottom-block">
-                    <div class="top-floor">
-                      <div class="time js-time-countdown"></div>
-                        @if ($promise->only_for_me)
-                            <div class="eye eye-cross" data-tooltip="Обещание защищено настройками приватности<br>и видимо только вам."></div>
-                        @else
-                            <div class="eye" data-tooltip="Обещание видно всем пользователям."></div>
-                        @endif
-                      <!-- <div class="eye{{ ( $promise->only_for_me ? ' eye-cross' : '') }}"></div> -->
-                    </div>
-                    <div class="bottom-floor">
-                      <!--
-                      <div class="views">15</div>
-                      -->
-                      <div class="comments" data-tooltip="{{ trans_choice(':count комментарий|:count комментария|:count комментариев', (int)$promise->comments_count, array(), 'ru') }} к этому обещанию.">{{ $promise->comments_count }}</div>
-
-                        <?
-                        $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
-                        ?>
-                        @if ($failed)
-                            <div class="unsmile" data-tooltip="Обещание не было выполнено.">
-                                <!-- <i class="fi icon-no"></i> -->
+                @foreach ($promises as $promise)
+                    <?
+                    ++$p;
+                    if (!$promise->style_id) {
+                        $styles = array('green', 'aqua', 'yellow', 'blue', 'pink');
+                        $promise->style_id = $styles[array_rand($styles)];
+                    }
+                    ?>
+                    <li class="promise-item type-{{ $promise->style_id }}" data-finish="{{ $promise->time_limit }}">
+                        <ul class="soc-block">
+                            <li>
+                                <a onclick="ga('send', 'event', 'like', 'facebook');" href="http://www.facebook.com/sharer.php?u={{ URL::route('app.promise', $promise->id) }}" class="soc-icon" target="_blank"><i class="fi icon-fb"></i></a>
+                            </li>
+                            <li>
+                                <a onclick="ga('send', 'event', 'like', 'vkontakte');" href="http://vk.com/share.php?url={{ URL::route('app.promise', $promise->id) }}&event=button_share" class="soc-icon" target="_blank"><i class="fi icon-vk"></i></a>
+                            </li>
+                            <li>
+                                <a onclick="ga('send', 'event', 'like', 'odnoklassniki');" href="http://www.odnoklassniki.ru/dk?st.cmd=addShare&st._surl={{ URL::route('app.promise', $promise->id) }}" class="soc-icon" target="_blank"><i class="fi icon-ok"></i></a>
+                            </li>
+                        </ul>
+                        <a href="{{ URL::route('app.promise', $promise->id) }}" class="promise-content">
+                            <div class="title">
+                                {{ $promise->name }}
                             </div>
-                        @elseif ($promise->finished_at)
-                            <div class="smile" data-tooltip="Обещание выполнено.">
-                                <!-- <i class="fi icon-okey"></i> -->
+                            <div class="bottom-block">
+                                <div class="top-floor">
+                                    <div class="time js-time-countdown"></div>
+                                    @if ($promise->only_for_me)
+                                        <div class="eye eye-cross" data-tooltip="Обещание защищено настройками приватности<br>и видимо только вам."></div>
+                                    @else
+                                        <div class="eye" data-tooltip="Обещание видно всем пользователям."></div>
+                                        @endif
+                                                <!-- <div class="eye{{ ( $promise->only_for_me ? ' eye-cross' : '') }}"></div> -->
+                                </div>
+                                <div class="bottom-floor">
+                                    <!--
+                                    <div class="views">15</div>
+                                    -->
+                                    <div class="comments" data-tooltip="{{ trans_choice(':count комментарий|:count комментария|:count комментариев', (int)$promise->comments_count, array(), 'ru') }} к этому обещанию.">{{ $promise->comments_count }}</div>
+
+                                    <?
+                                    $failed = !$promise->finished_at && ($promise->promise_fail || date('Y-m-d H:i:s') > $promise->time_limit);
+                                    ?>
+                                    @if ($failed)
+                                        <div class="unsmile" data-tooltip="Обещание не было выполнено.">
+                                            <!-- <i class="fi icon-no"></i> -->
+                                        </div>
+                                    @elseif ($promise->finished_at)
+                                        <div class="smile" data-tooltip="Обещание выполнено.">
+                                            <!-- <i class="fi icon-okey"></i> -->
+                                        </div>
+                                        @else
+                                                <!-- <div class="time">02:01:23</div> -->
+                                    @endif
+
+                                </div>
                             </div>
-                        @else
-                            <!-- <div class="time">02:01:23</div> -->
-                        @endif
+                        </a>
+                    </li>
 
-                    </div>
-                  </div>
-                </a>
-              </li>
+                    @if (@$p == 2)
+                        @yield('koe-chto')
+                    @endif
 
-                @if (@$p == 2)
+                @endforeach
+
+                @if (@$p == 1)
                     @yield('koe-chto')
                 @endif
-
-            @endforeach
-
-            @if (@$p == 1)
-                @yield('koe-chto')
-            @endif
 
             </ul>
         @else
 
             <div class="wrapper">
-              <div class="us-text">Этот пользователь уже зарегистрирован на сайте, но пока не давал обещаний.</div>
+                <div class="us-text">Этот пользователь уже зарегистрирован на сайте, но пока не давал обещаний.</div>
             </div>
-            
+
         @endif
 
-    <script>
-        var __SITE__ = {};
-        __SITE__.achievements = {{ json_encode($ach_list, JSON_UNESCAPED_UNICODE) }};
-    </script>
+        <script>
+            var __SITE__ = {};
+            __SITE__.achievements = {{ json_encode($ach_list, JSON_UNESCAPED_UNICODE) }};
+        </script>
 
 @stop
 
